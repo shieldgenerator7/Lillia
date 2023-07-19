8b4342fce46c5360244395cc2198b435c75d454e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-07-19 04:17:11 -0400

    Fruits respawn on death

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class Hazard : MonoBehaviour
@@ -7,5 +8,6 @@ public abstract class Hazard : MonoBehaviour
     protected void killPlayer()
     {
         FindObjectOfType<CheckpointManager>().teleportToCurrent();
+        FindObjectsOfType<Fruit>().ToList().ForEach(fruit => fruit.Available = true);
     }
 }
