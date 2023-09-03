811478fa2fe0d475bd8f2d2792ab546a96f50b8e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-09-03 07:09:52 -0400

    Fix bug: warwick not damaging player

--- Assets/Scripts/StaticHazard.cs
+++ Assets/Scripts/StaticHazard.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class StaticHazard : Hazard
 {
+    public bool onCollide = false;
+    public bool onTrigger = true;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            killPlayer();
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
