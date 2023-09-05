65610548e3140651201a5009bc73f8110ad13a96
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-09-05 01:05:35 -0400

    Organize PlayerAttributes

--- Assets/Scripts/Player/PlayerAttributes.cs
+++ Assets/Scripts/Player/PlayerAttributes.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerAttributes", menuName = "Player/PlayerAttributes")]
 public class PlayerAttributes : ScriptableObject
 {
+    [Header("Prance")]
     public float walkSpeed;
     public float runSpeedPerStack;
     public int maxStacks;
-    public float jumpForce;
-    public float coyoteTime = 0.1f;
     public float stackDecayDelay;
     public float stackDecayDelayPerStack;
+    [Header("Jump")]
+    public float jumpForce;
+    public float coyoteTime = 0.1f;
+    [Header("Blooming Blows")]
     public int maxAirBloomingBlows;
     public float airBloomingBlowsAntiGravDuration;
-    public Vector2 swirlSeedLaunchVector;
-    public float swirlSeedPickupRange = 2;
+    [Header("Watch out! Eep!")]
     public int maxAirJumps;
     public float momentumChangeFactor;
     public float momentumInstantSnapThreshold;
     public float jumpForcePerStack;
+    [Header("Swirlseed")]
+    public Vector2 swirlSeedLaunchVector;
+    public float swirlSeedPickupRange = 2;
+    [Header("Wall Bounce")]
     public float wallBounceSpeedUpFactor;
     public float wallBounceJumpFactor;
     public float wallBounceDuration;
