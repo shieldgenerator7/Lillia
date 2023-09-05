91cfc43d40194750d53f8a64b6f532ab0b14ea7e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-09-05 05:08:12 -0400

    Gut Checkpoint

--- Assets/Scripts/Mechanics/Checkpoint.cs
+++ Assets/Scripts/Mechanics/Checkpoint.cs
@@ -5,24 +5,6 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    [Tooltip("True if the player is allowed to teleport here after death")]
-    public bool canTeleportHere = true;
-
-    public Color activeColor = Color.white;
-    public Color inactiveColor = Color.white;
-
-    private SpriteRenderer sr;
-
-    public void markCurrent(bool current = true)
-    {
-        sr.color = (current) ? activeColor : inactiveColor;
-    }
-
-    private void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>())
