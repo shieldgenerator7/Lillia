0cf7797b29f1029f3a90b4cc24aea67f49cf7b0a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-09-05 06:00:25 -0400

    Fruits can be collected by walking into them

--- Assets/Scripts/Mechanics/Hittable.cs
+++ Assets/Scripts/Mechanics/Hittable.cs
@@ -6,6 +6,8 @@ public sealed class Hittable : Resettable
 {
     public int stacksGranted = 1;
     public bool collectable = true;
+    [Tooltip("True if it can be 'hit' just by walking into it")]
+    public bool passiveable = true;
 
     [Tooltip("When it gets hit, it hides this collider")]
     public Collider2D hideCollider;
@@ -35,6 +37,18 @@ public sealed class Hittable : Resettable
         if (hideCollider)
         {
             hideCollider.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (passiveable)
+        {
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            if (playerController)
+            {
+                playerController.ProcessHittable(this);
+            }
         }
     }
 
