a2bf558e5c37b3a8415990a89078a67852182c31
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-09-11 08:46:57 -0400

    Reorganize LevelInfo

--- Assets/Scripts/Data/LevelInfo.cs
+++ Assets/Scripts/Data/LevelInfo.cs
@@ -8,18 +8,14 @@ public class LevelInfo : ScriptableObject
 {
     [Tooltip("The name of the level to display on screen")]
     public string levelName;
+    [Tooltip("The level id to use in the save file")]
+    public string id;
 #if UNITY_EDITOR
     [Tooltip("The scene object to link (editor only)")]
     public SceneAsset scene;
 #endif
-    [Tooltip("The name of the scene asset to load (generated)")]
-    public string sceneName;
-    [Tooltip("The level id to use in the save file")]
-    public string id;
     [Tooltip("Where the player spawns in the level")]
     public Vector3 startPos;
-    [Tooltip("How many collectibles in this area (generated)")]
-    public int collectibleCount;
 
     [Header("Abilities")]
     public bool bloomingBlows = true;
@@ -27,4 +23,10 @@ public class LevelInfo : ScriptableObject
     public bool watchOutEepSlam = true;
     public bool swirlseed = true;
     public bool liltingLullaby = true;
+
+    [Header("Auto-Regenerated (Do NOT manually edit!)")]
+    [Tooltip("The name of the scene asset to load (generated)")]
+    public string sceneName;
+    [Tooltip("How many collectibles in this area (generated)")]
+    public int collectibleCount;
 }
